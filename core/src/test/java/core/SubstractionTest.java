2ce74c77599b2fe0de540fdcc83d2366eb6487e3
Allan <Allan@192.168.6.157>
2016-09-06 16:21:29 +0200

    Ajout classe de test des soustraction - Test d'une soustraction

---
+++ core/src/test/java/core/SubstractionTest.java
@@ -0,0 +1,14 @@
+package core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SubstractionTest {
+
+    @Test
+    public void testCalculate() throws DivisonByZeroException {
+        assertEquals(0, DivisionMachine.getInstance().calculate(2, 2), 0);
+    }
+
+}
